a91e33e2ae92b856a68fe174e0dda3fce3b61715
Y8a0 <halidouyacouba999@gmail.com>
2023-10-26 12:16:08 +0100

    Deploiement

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
+
 // Récupère l'élément d'affichage par son ID et initialise la variable calculatrice.
 let nombre = document.getElementById('display');
 let calculater = '';
+let hasDecimal = false; // Flag to track if a decimal point has been used.
 
 // Fonction pour ajouter une valeur à l'entrée de la calculatrice.
 function appendValue(valeur) {
+    if (valeur === '.' && hasDecimal) {
+        return; // Prevent adding more than one decimal point.
+    }
+
+    if (valeur === '.') {
+        hasDecimal = true;
+    }
+
     calculater = calculater + valeur; // Ajoute la valeur à la chaîne de saisie de la calculatrice.
     nombre.value = calculater; // Met à jour l'affichage avec la nouvelle saisie.
 }
 
 // Fonction pour supprimer le dernier caractère de l'entrée.
 function deleteLastCharacter() {
+    if (calculater.charAt(calculater.length - 1) === '.') {
+        hasDecimal = false;
+    }
+
     calculater = calculater.slice(0, -1); // Supprime le dernier caractère de la chaîne de saisie.
     nombre.value = calculater; // Met à jour l'affichage avec la saisie modifiée.
 }
@@ -18,54 +32,72 @@ function deleteLastCharacter() {
 function clearDisplay() {
     calculater = ''; // Réinitialise la chaîne de saisie de la calculatrice.
     nombre.value = calculater; // Efface l'affichage.
+    hasDecimal = false; // Reset the decimal flag.
 }
 
 // Fonction pour effectuer des calculs basés sur l'expression d'entrée.
 function calculate() {
     try {
-        // Utilise la fonction evaluateExpression pour calculer le résultat.
-        const resultat = evaluateExpression(calculater);
+        const resultat = calcul(calculater);
         nombre.value = resultat; // Affiche le résultat sur la calculatrice.
         calculater = resultat.toString(); // Met à jour la saisie avec le résultat.
+        hasDecimal = false; // Reset the decimal flag after calculation.
     } catch (erreur) {
         nombre.value = 'Erreur'; // Affiche un message d'erreur sur la calculatrice en cas d'erreur.
         calculater = ''; // Réinitialise la saisie.
     }
 }
 
-// Fonction pour évaluer l'expression d'entrée et effectuer des calculs sécurisés.
-function evaluateExpression(expression) {
-    const operateurs = ['+', '-', '*', '/'];
-    const valeurs = expression.split(/\b/);
+// Fonction pour évaluer l'expression de manière sécurisée
+function calcul(expression) {
+    const operators = ['+', '-', '*', '/'];
+    const numStack = [];
+    const opStack = [];
+    let currentNum = '';
 
-    // Divise l'expression en opérateurs et en valeurs.
-    const pile = [];
-    let operateurCourant = '+';
+    for (let i = 0; i < expression.length; i++) {
+        const char = expression[i];
 
-    for (const item of valeurs) {
-        if (operateurs.includes(item)) {
-            operateurCourant = item;
-        } else {
-            const valeur = parseFloat(item);
-            if (!isNaN(valeur)) {
-                if (operateurCourant === '+') {
-                    pile.push(valeur);
-                } else if (operateurCourant === '-') {
-                    pile.push(-valeur);
-                } else if (operateurCourant === '*') {
-                    const valeurPrecedente = pile.pop();
-                    pile.push(valeurPrecedente * valeur);
-                } else if (operateurCourant === '/') {
-                    const valeurPrecedente = pile.pop();
-                    pile.push(valeurPrecedente / valeur);
-                }
+        if (/\d|\./.test(char)) {
+            currentNum += char;
+        } else if (operators.includes(char)) {
+            numStack.push(parseFloat(currentNum));
+            currentNum = '';
+
+            while (
+                opStack.length > 0 &&
+                operators.indexOf(opStack[opStack.length - 1]) >= operators.indexOf(char)
+            ) {
+                numStack.push(applyOperator(opStack.pop(), numStack.pop(), numStack.pop()));
             }
+
+            opStack.push(char);
         }
     }
 
-    // Calcule le résultat en additionnant les valeurs dans la pile.
-    const resultat = pile.reduce((acc, val) => acc + val, 0);
-    return resultat; // Renvoie le résultat calculé.
+    numStack.push(parseFloat(currentNum));
+
+    while (opStack.length > 0) {
+        numStack.push(applyOperator(opStack.pop(), numStack.pop(), numStack.pop()));
+    }
+
+    return numStack[0];
+}
+
+function applyOperator(operator, b, a) {
+    switch (operator) {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            if (b === 0) {
+                throw 'Division by zero';
+            }
+            return a / b;
+    }
 }
 
 // Écouteur d'événements pour l'entrée au clavier et les changements de thème.
@@ -76,16 +108,18 @@ document.addEventListener("keydown", function(event){
     const key = event.key;
     if (/[0-9]/.test(key)) {
         appendValue(key);
-    }else if (key === "+" || key === "-" || key === "*" || key === "/") {
+    } else if (key === "+" || key === "-" || key === "*" || key === "/") {
         appendValue(key);
-    }else if (key === "Backspace") {
+    } else if (key === "." && !hasDecimal) {
+        appendValue(key);
+    } else if (key === "Backspace") {
         deleteLastCharacter();
-    }else if (key === "Delete"){
+    } else if (key === "Delete") {
         clearDisplay();
-    }else if (key === "Enter") {
+    } else if (key === "Enter") {
         calculate();
     }
-})
+});
 
 // Les fonction qui permet de les echange de thème
 const theme1 = document.querySelector('#theme1');
@@ -153,43 +187,44 @@ theme2.addEventListener("click", (e) =>{
                 memeCouleur[i].style.backgroundColor = "#e9e9ea";  
            }
            for (let i = 0; i < memeCoul.length; i++) {
-               memeCoul[i].style.backgroundColor = "#3d828c";  
-          }
-          for (let i = 0; i < uniqueCouleur.length; i++) {
-           uniqueCouleur[i].style.backgroundColor = "#ad500a";  
-      }
-          } else {
-            
-          }  
-  });
-
-  theme3.addEventListener("click", (e) =>{
-    if (e.target.checked) {
-        boutton.style.backgroundColor = "#21043e";
-        mpo.style.backgroundColor = "#21043e";
-        calculatrice.style.color = "#e5c753";
-        libele.style.backgroundColor = "#21043e";
-        displayer.style.backgroundColor = "#21043e";
-        document.body.style.backgroundColor = "#18012e";
-        container.style.backgroundColor = "#18012e";
-        themeText.style.backgroundColor = "#18012e";
-        theme3.style.backgroundColor = '#07b9b2';
-        theme1.style.backgroundColor = '#21043e';
-        theme2.style.backgroundColor = '#21043e';
-
-        
-        for (let i = 0; i < memeCouleur.length; i++) {
-             memeCouleur[i].style.color = "#e5c753";  
-             memeCouleur[i].style.backgroundColor = "#5d0481";  
-        }
-        for (let i = 0; i < memeCoul.length; i++) {
-            memeCoul[i].style.backgroundColor = "#18012e";  
+            memeCoul[i].style.backgroundColor = "#3d828c";  
        }
        for (let i = 0; i < uniqueCouleur.length; i++) {
-        uniqueCouleur[i].style.backgroundColor = "#07b9b2";  
+        uniqueCouleur[i].style.backgroundColor = "#ad500a";  
    }
-      } else {
-        
-      }  
+       } else {
+         
+       }  
 });
+
+theme3.addEventListener("click", (e) =>{
+ if (e.target.checked) {
+     boutton.style.backgroundColor = "#21043e";
+     mpo.style.backgroundColor = "#21043e";
+     calculatrice.style.color = "#e5c753";
+     libele.style.backgroundColor = "#21043e";
+     displayer.style.backgroundColor = "#21043e";
+     document.body.style.backgroundColor = "#18012e";
+     container.style.backgroundColor = "#18012e";
+     themeText.style.backgroundColor = "#18012e";
+     theme3.style.backgroundColor = '#07b9b2';
+     theme1.style.backgroundColor = '#21043e';
+     theme2.style.backgroundColor = '#21043e';
+
+     
+     for (let i = 0; i < memeCouleur.length; i++) {
+          memeCouleur[i].style.color = "#e5c753";  
+          memeCouleur[i].style.backgroundColor = "#5d0481";  
+     }
+     for (let i = 0; i < memeCoul.length; i++) {
+         memeCoul[i].style.backgroundColor = "#18012e";  
+    }
+    for (let i = 0; i < uniqueCouleur.length; i++) {
+     uniqueCouleur[i].style.backgroundColor = "#07b9b2";  
+}
+   } else {
+     
+   }  
+});
+
 
